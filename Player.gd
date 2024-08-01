7a21066814ddd521e14716f3017522c4eb659a07
wnskim <wnskim@gmail.com>
2024-08-01 06:32:29 -0400

    fixed up shooting mechanics

--- Player.gd
+++ Player.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
 
-var shooting:bool
+var mouse_button_pressed:bool
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	velocity = direction * 500
 	move_and_slide()
 	
-	if velocity.length() > 0.0 and shooting == false:
+	if velocity.length() > 0.0 and mouse_button_pressed == false:
 		rotation = velocity.angle()
 	
-
+"""
 func _input(event):
 	if event.is_action_pressed("click"):
-		shooting = true
+		mouse_button_pressed = true
 		rotate(get_angle_to(get_global_mouse_position()))
 		await get_tree().create_timer(0.1).timeout
-		shooting = false
+		mouse_button_pressed = false
+	"""
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():  # Mouse button down.
+			mouse_button_pressed = true
+		elif not event.is_pressed():  # Mouse button released.
+			mouse_button_pressed = false
+
+func _process(delta):
+	if mouse_button_pressed: 
+		rotate(get_angle_to(get_global_mouse_position()))
--- gun.gd
+++ gun.gd
@@ -7,8 +7,13 @@ func shoot():
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
 	
-
+"""
 func _input(event):
 	if event.is_action_pressed("click"):
-		await get_tree().create_timer(0.1).timeout
+		shoot()
+"""
+
+
+func _on_timer_timeout():
+	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
 		shoot()

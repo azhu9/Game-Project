e0da4b944df1f2f4cc2934ef6e41b7d5bf3087f1
azhu9 <109678994+azhu9@users.noreply.github.com>
2024-08-04 15:18:04 -0400

    Added zoom function

--- Player.gd
+++ Player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var mouse_button_pressed:bool
+#variables for camera zoom
+var zoom_minimum = Vector2(5.00001, 5.00001)
+var zoom_maximum = Vector2(2.00001, 2.00001)
+var zoom_speed = Vector2(.100001, .100001)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
-	velocity = direction * 500
+	velocity = direction * 300
 	move_and_slide()
 	
 	if velocity.length() > 0.0 and mouse_button_pressed == false:
@@ -18,6 +22,13 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():  # Mouse button down.
 			mouse_button_pressed = true
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP: #Zoom in when MW up
+				if $Camera2D.zoom < zoom_minimum:
+					$Camera2D.zoom += zoom_speed
+					pass
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN: #Zoom in when MW down
+				if $Camera2D.zoom > zoom_maximum:
+					$Camera2D.zoom -= zoom_speed
 		elif not event.is_pressed():  # Mouse button released.
 			mouse_button_pressed = false
 
